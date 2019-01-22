405883cbff4ed632ed6255ed788158422a40384e
Stepan Rodionov <stepanhero111@gmail.com>
2019-01-22 21:49:27 +0300

    Changed Coordinates file

--- src/Coordinates.php
+++ src/Coordinates.php
@@ -2,7 +2,23 @@
 
 namespace SR\GeoDataTest;
 
+use PDO;
+
 class Coordinates
 {
+    /**
+     * @var PDO
+     */
+    private $connection;
 
+    /**
+     * @var string
+     */
+    private $tableName;
+
+    public function __construct(PDO $connection, string $tableName)
+    {
+        $this->connection = $connection;
+        $this->tableName = $tableName;
+    }
 }
