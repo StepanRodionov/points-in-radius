c09ddb1bb05a07b6fb74926345ce5b138e37ec10
Stepan Rodionov <stepanhero111@gmail.com>
2019-01-27 20:08:13 +0300

    Убрал процедуру - python ее не переваривает

--- assets/generate.py
+++ assets/generate.py
@@ -15,36 +15,6 @@ cursor = conn.cursor()
 cursor.execute("drop table if exists points");
 conn.commit()
 
-geoproc = """
-DELIMITER $
-DROP FUNCTION IF EXISTS distance_between $
-CREATE FUNCTION distance_between (
-  tbl_lat double, tbl_lon double,
-  lat double, lon double
-) RETURNS DECIMAL(6,2) DETERMINISTIC
-BEGIN
-
-  SET @dist := 6371000 * 2 * ASIN(
-    SQRT(
-        POWER(
-          SIN((tbl_lat - ABS(lat)) * PI() / 180 / 2),
-          2
-        ) +
-        COS(tbl_lat * PI() / 180) *
-        COS(ABS(lat) * PI() / 180) *
-        POWER(
-          SIN((tbl_lon - lon) * PI() / 180 / 2),
-          2
-        )
-    )
-  );
-  RETURN @dist;
-END $
-DELIMITER ;
-"""
-cursor.execute(geoproc)
-conn.commit()
-
 
 create = """
 create table points
