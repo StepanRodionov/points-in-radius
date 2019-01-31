7516e99038e0b18afa1eb01dfb17946cc7fb22f8
Stepan Rodionov <stepanhero111@gmail.com>
2019-02-01 00:00:25 +0300

    Заменил библиотеку python

--- assets/generate.py
+++ assets/generate.py
@@ -1,4 +1,5 @@
-import MySQLdb
+import pymysql
+import pymysql.cursors
 import settings
 import math
 from random import shuffle
@@ -7,9 +8,9 @@ from scipy.stats import norm as norm
 lat = norm.rvs(loc=55.75, scale=0.09, size=settings.set_size, random_state=29)
 lon = norm.rvs(loc=37.62, scale=0.107, size=settings.set_size, random_state=72)
 
-many_coords = [(float(lat[x]), float(lon[x]), float(math.cos(math.radians(lat[x])))) for x in range(0, len(lat))]
+many_coords = [(float(lat[x]), float(lon[x]), float(math.cos(math.radians(lat[x]))), float(lat[x]), float(lon[x])) for x in range(0, len(lat))]
 
-conn = MySQLdb.connect(db=settings.db, host=settings.host, port=settings.port, user=settings.user, passwd=settings.passwd)
+conn = pymysql.connect(db=settings.db, host=settings.host, port=settings.port, user=settings.user, passwd=settings.passwd, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 cursor = conn.cursor()
 
 cursor.execute("drop table if exists points");
@@ -23,6 +24,7 @@ create table points
 	lat double not null,
 	lon double not null,
 	lat_cos double not null,
+	g GEOMETRY NOT NULL SRID 4326, SPATIAL INDEX(g),
 	constraint points_pk
 		primary key (id)
 );"""
@@ -36,7 +38,7 @@ create index simple__index
 cursor.execute(setindex)
 conn.commit()
 
-insert = """insert into points(lat, lon, lat_cos) values (%s, %s, %s)"""
+insert = """insert into points(lat, lon, lat_cos, g) values (%s, %s, %s, ST_GeomFromText('POINT(%s %s)', 4326))"""
 cursor.executemany(insert, many_coords)
 
 cursor.close()
